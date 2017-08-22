b89b5b1df60d8c39e249aa703636b27e8270e2f8
Lucy Bain <lbain@atlassian.com>
2017-08-22 20:19:57 +1000

    Display messenger checkbox

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,7 +1,13 @@
 require "facebook/messenger"
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: Figaro.env.facebook_access_token)
+Facebook::Messenger::Subscriptions.subscribe(access_token: Figaro.env.facebook_page_access_token)
 Bot.on :message do |message|
-    puts message.text
+    Bot.deliver({
+        recipient: message.sender,
+        message: {
+          text: message.text
+        }
+      }, access_token: Figaro.env.facebook_page_access_token)
 end
+
